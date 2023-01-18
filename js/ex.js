35e911b3632a2e14784df6bc4386e100fec8d3bb
Majoonho <jofmjh@gmail.com>
2023-01-18 14:50:27 +0900

    fix: 카카오맵 마커표시 안되던 기능 수정

--- js/ex.js
+++ js/ex.js
@@ -19,22 +19,24 @@ function setPointsFromJson() {
 
     var temp = 
       {
-          content: `<div>${title}/div>`, 
-          latlng: new kakao.maps.LatLng(x, y)
+          content: `<div style="font-size: 13px; padding:4px; text-align:center; width:150px">${title} 영업소</div>`, 
+          latlng: new kakao.maps.LatLng(y, x)
       };
     positions.push(temp)
 
 	}
 
+  // console.log(positions)
+
 
 
   var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
   mapOption = { 
-      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-      level: 3 // 지도의 확대 레벨
+      center: new kakao.maps.LatLng(37.396399, 127.1032), // 지도의 중심좌표
+      level: 10 // 지도의 확대 레벨
   };
 
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+  var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
 // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다 
 // var positions = [
@@ -56,7 +58,7 @@ var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니
 //   }
 // ];
 
-console.log(positions[0].content )
+// console.log(positions[0].content )
 
 for (var i = 0; i < positions.length; i ++) {
   // 마커를 생성합니다
@@ -73,7 +75,7 @@ for (var i = 0; i < positions.length; i ++) {
   // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
   // 이벤트 리스너로는 클로저를 만들어 등록합니다 
   // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-  kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+  kakao.maps.event.addListener(marker, 'click', makeOverListener(map, marker, infowindow));
   kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
 }
 
